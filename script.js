54f0c42fbc94246d3b461f89473ce38306d22d40
Ulaana <157713810+Ulaana@users.noreply.github.com>
2024-05-28 17:07:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -124,7 +124,6 @@ function findNearestStation(e) {
             routeWhileDragging: true,
             language: 'pl'
         }).addTo(map);
-        nearestStation.bindPopup(popupContent);
     }
 }
 
