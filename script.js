618fbfb9072d7e9210f6439b28c0fd268eaee395
Ulaana <157713810+Ulaana@users.noreply.github.com>
2024-05-28 16:39:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ var redIcon = L.icon({
                 iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
                 shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
                 iconSize: [25, 41],
+                shadowSize: [41, 41],
                 iconAnchor: [12, 41],
-                popupAnchor: [1, -34],
-                shadowSize: [41, 41]
+                shadowAnchor: [24, 41],
+                popupAnchor: [1, -34]
 });
 
 Promise.all([
